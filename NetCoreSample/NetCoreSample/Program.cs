3f5a844408b711608ef84d1b6abd441f95f3e417
beyazc <cahitbyz@gmail.com>
2017-09-14 17:25:41 +0300

    add code

--- NetCoreSample/NetCoreSample/Program.cs
+++ NetCoreSample/NetCoreSample/Program.cs
@@ -8,6 +8,26 @@ namespace NetCoreSample
     {
         static void Main(string[] args)
         {
+            #region Assembly.LoadFile debug comment below region to debug this way
+            //var assemlies = Assembly.GetEntryAssembly().GetReferencedAssemblies();
+            //var assemblyName = "";
+            //foreach (var item in assemlies)
+            //{
+            //    if (item.Name == "RefProj")
+            //        assemblyName = item.Name;
+
+            //}
+            //var a = Assembly.Load(assemblyName);
+            //var t = a.GetType("RefProj.Class1");
+            //var i = (RefProj.Class1)Activator.CreateInstance(t);
+            //i.get1();//press f11
+
+            #endregion
+
+
+
+            #region Assembly.LoadWay comment above and uncommet below
+
             var assemlies = Assembly.GetEntryAssembly().GetReferencedAssemblies();
             var assemblyName = "";
             foreach (var item in assemlies)
@@ -16,21 +36,15 @@ namespace NetCoreSample
                     assemblyName = item.Name;
 
             }
-            //var a = Assembly.Load(assemblyName);
             var path = new FileInfo(Assembly.GetExecutingAssembly().Location);
             var a = Assembly.LoadFile(Path.Combine($@"{path}\..\..\..\..\..\RefProj\bin\Debug\netstandard2.0", "RefProj.dll"));
-
             var t = a.GetType("RefProj.Class1");
+
             var i = Activator.CreateInstance(t);
             MethodInfo mi = t.GetMethod("get1");
             mi.Invoke(i, null);
 
-
-
-            // Assuming the method returns a boolean and accepts a single string parameter
-            //bool rc = Convert.ToBoolean(mi.Invoke(moduleInstance.Unwrap(), new object[] { "MyParamValue" }));
-
-            //var resp = i.get1();
+            #endregion
 
         }
     }
